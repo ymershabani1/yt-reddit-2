25662a0020cf2d38f465c3480598305bed86e87a
ymershabani1 <72756150+ymershabani1@users.noreply.github.com>
2022-07-04 23:54:06 +0200

    Implementing Next-Auth

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { SessionProvider } from 'next-auth/react'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+  return (
+    <SessionProvider session={pageProps.session}>
+      <div>
+      <Component {...pageProps} />
+      </div>  
+    </SessionProvider>
+  )
 }
 
 export default MyApp
